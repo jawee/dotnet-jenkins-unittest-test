78d1704fdb62a09ccc12749cec78d509e29e33d9
Andreas Olsson <olsson.andreas@gmail.com>
2021-09-30 17:07:00 +0200

    Test should pass

--- MyLibrary/MyLibraryClass.cs
+++ MyLibrary/MyLibraryClass.cs
@@ -6,6 +6,10 @@ namespace MyLibrary
     {
       public bool IsPrime(int candidate) 
       {
+        if(candidate == 1)
+        {
+          return false;
+        }
         throw new NotImplementedException("Not implemented");
       }
     }
